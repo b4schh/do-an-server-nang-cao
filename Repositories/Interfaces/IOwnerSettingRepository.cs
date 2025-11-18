b6bf220d61822e1c58f063b2416848182cc9ae19
Hoang <hoangarmy4@gmail.com>
2025-11-18 20:32:15 +0700

    Thêm chức năng CRUD cho OwnerSetting

--- Repositories/Interfaces/IOwnerSettingRepository.cs
+++ Repositories/Interfaces/IOwnerSettingRepository.cs
@@ -4,7 +4,7 @@ namespace FootballField.API.Repositories.Interfaces
 {
     public interface IOwnerSettingRepository : IGenericRepository<OwnerSetting>
     {
-        // Nếu có thêm hàm đặc thù thì khai báo ở đây
+
         Task<OwnerSetting?> GetByOwnerIdAsync(int ownerId);
     }
 }
